c1e413c1477bec927ca9c3a5513b749f1fe7281a
Sahinur Islam <infosahinur@gmail.com>
2022-03-11 02:47:44 +0600

    api set done

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,102 +3,109 @@ import SmallCard from '../components/SmallCard'
 import Marquee from 'react-fast-marquee'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-const takaPay = [
-  {
-    id: '1',
-    month: 'January',
-    pay: true,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '2',
-    month: 'February ',
-    pay: true,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '3',
-    month: 'March',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '4',
-    month: 'April',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '5',
-    month: 'May ',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '6',
-    month: 'June ',
-    pay: true,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '7',
-    month: 'July ',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '8',
-    month: 'August',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '9',
-    month: 'September',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '10',
-    month: 'October',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '11',
-    month: 'November ',
-    pay: false,
-    payData: '22-03-2020',
-    price: '700',
-  },
-  {
-    id: '12',
-    month: 'December ',
-    pay: true,
-    payData: '22-03-2020',
-    price: '700',
-  },
-]
+// const takaPay = [
+//   {
+//     id: '1',
+//     month: 'January',
+//     pay: true,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '2',
+//     month: 'February ',
+//     pay: true,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '3',
+//     month: 'March',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '4',
+//     month: 'April',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '5',
+//     month: 'May ',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '6',
+//     month: 'June ',
+//     pay: true,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '7',
+//     month: 'July ',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '8',
+//     month: 'August',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '9',
+//     month: 'September',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '10',
+//     month: 'October',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '11',
+//     month: 'November ',
+//     pay: false,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+//   {
+//     id: '12',
+//     month: 'December ',
+//     pay: true,
+//     payData: '22-03-2020',
+//     price: '700',
+//   },
+// ]
 
 function SeatBooking() {
   const router = useRouter()
   const { data: session } = useSession()
+  const [pay, setPay] = useState([])
 
-  useEffect(()=>{
+  useEffect(async () => {
+    const res = await fetch(`http://localhost:3000/api/user`)
+    const data = await res.json()
+    setPay(data.Login[0].payData)
+  }, [])
+
+  useEffect(() => {
     !session && router.push('/')
-  },[session])
+  }, [session])
   return (
     <div>
       <Header />
@@ -110,15 +117,14 @@ function SeatBooking() {
         **** বর্তমানে হোস্টেলে চারটা সিট ফাঁকা আছে ****
       </Marquee>
       <section className="mx-auto max-w-7xl pt-6">
-        <h2 className="pb-5 text-4xl font-semibold">Your bill payment activity</h2>
+        <h2 className="pb-5 text-4xl font-semibold">
+          Your bill payment activity
+        </h2>
 
         {/* Pull some data from a server - API endPoints */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {takaPay?.map((bill) => (
-            <SmallCard
-              key={bill.id}
-              bill={bill}
-            />
+          {pay?.map((bill) => (
+            <SmallCard key={bill.id} bill={bill} />
           ))}
         </div>
       </section>
