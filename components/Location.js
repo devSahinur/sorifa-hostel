12ceb019ffb54960ed9a045c2ba4563053debff5
Sahinur Islam <infosahinur@gmail.com>
2022-03-10 11:37:07 +0600

    home page responsive

--- components/Location.js
+++ components/Location.js
@@ -55,7 +55,7 @@ function Location() {
               className="mb-4 h-48 w-48 flex-shrink-0 rounded-lg object-cover object-center sm:mb-0"
               src="https://dummyimage.com/200x200"
             />
-            <div className="flex-grow sm:pl-8">
+            <div className="flex-grow ml-3  md:ml-0 lg:ml-0  sm:pl-8">
               <h2 className="title-font text-lg font-medium text-gray-900">
                 Holden Caulfield
               </h2>
@@ -64,56 +64,16 @@ function Location() {
                 DIY tote bag drinking vinegar cronut adaptogen squid fanny pack
                 vaporware.
               </p>
-              <span className="inline-flex">
-                <a className="text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-5 w-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a className="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-5 w-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a className="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-5 w-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span>
+
             </div>
           </div>
-          <div className="inline-flex">
+          <div className="inline-flex pt-5">
             <img
               alt="team"
               className="mb-4 h-48 w-48 flex-shrink-0 rounded-lg object-cover object-center sm:mb-0"
               src="https://dummyimage.com/200x200"
             />
-            <div className="flex-grow sm:pl-8">
+            <div className="flex-grow ml-3 md:ml-0 lg:ml-0 sm:pl-8">
               <h2 className="title-font text-lg font-medium text-gray-900">
                 Holden Caulfield
               </h2>
